1c42fd93aaf73fceba8ac2b53131dcbc4b7b880c
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-19 20:52:59 -0300

    Adicionando model Author

---
+++ OceanOfLettersAPI/Models/Author.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace OceanOfLettersAPI.Models
+{
+
+    public class Author
+    {
+
+        #region Properties
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("birth")]
+        public DateTime Birth { get; set; }
+
+        [JsonProperty("death")]
+        public DateTime? Death { get; set; }
+
+        [JsonProperty("biography")]
+        public string Biography { get; set; }
+
+        [JsonProperty("nationality")]
+        public string Nationality { get; set; }
+
+        [JsonProperty("occupation")]
+        public string Occupation { get; set; }
+
+        [JsonProperty("activity_period")]
+        public string ActivityPeriod { get; set; }
+
+        [JsonProperty("country_id")]
+        public int? CountryId { get; set; }
+
+        #endregion
+
+    }
+
+}
