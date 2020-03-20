3f0507fe99b15fa2a3b9ce2096e26d26c7de9073
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-20 15:25:58 -0300

    Adicionando model Series

---
+++ OceanOfLettersAPI/Models/Series.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace OceanOfLettersAPI.Models
+{
+
+    public class Series
+    {
+
+        #region Properties
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("synopsis")]
+        public string Synopsis { get; set; }
+
+        #endregion
+
+    }
+
+}
