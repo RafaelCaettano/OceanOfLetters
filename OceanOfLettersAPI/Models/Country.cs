fd1635bcf36855b7d0b7d3b9aa039ddab038deeb
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-20 14:00:58 -0300

    Adicionando model Country

---
+++ OceanOfLettersAPI/Models/Country.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace OceanOfLettersAPI.Models
+{
+
+    public class Country
+    {
+
+        #region Properties
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("language_id")]
+        public int? LanguageId { get; set; }
+
+        #endregion
+
+    }
+
+}
