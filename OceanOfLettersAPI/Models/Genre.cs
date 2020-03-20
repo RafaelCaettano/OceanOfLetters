74ed432badf07f00e164fd9ed5b71c8c236e112b
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-20 17:25:00 -0300

    Adicionando Navigation Properties da model Genre

--- OceanOfLettersAPI/Models/Genre.cs
+++ OceanOfLettersAPI/Models/Genre.cs
@@ -1,10 +1,29 @@
 ﻿using Newtonsoft.Json;
+using OceanOfLettersAPI.Models.Relationships;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OceanOfLettersAPI.Models
 {
 
     public class Genre
     {
+
+        public Genre()
+        {
+
+            GenresBooks = new HashSet<GenresBook>();
+            Books = new List<Book>();
+            GenresAuthors = new HashSet<GenresAuthor>();
+            Authors = new List<Author>();
+            GenresPublishingCompanies = new HashSet<GenresPublishingCompany>();
+            PublishingCompanies = new List<PublishingCompany>();
+            Brands = new List<Brand>();
+            GenresBrands = new HashSet<GenresBrand>();
+            GenresSeries = new HashSet<GenresSeries>();
+            Series = new List<Series>();
+
+        }
 
         #region Properties
 
@@ -16,6 +35,45 @@ namespace OceanOfLettersAPI.Models
 
         [JsonProperty("description")]
         public string Description { get; set; }
+
+        #endregion
+
+        #region Navigation Properties
+
+        [JsonProperty("books")]
+        [NotMapped]
+        public ICollection<Book> Books { get; set; }
+
+        [JsonProperty("authors")]
+        [NotMapped]
+        public ICollection<Author> Authors { get; set; }
+
+        [JsonProperty("brands")]
+        [NotMapped]
+        public ICollection<Brand> Brands { get; set; }
+
+        [JsonProperty("publishing_companies")]
+        [NotMapped]
+        public ICollection<PublishingCompany> PublishingCompanies { get; set; }
+
+        [JsonProperty("series")]
+        [NotMapped]
+        public ICollection<Series> Series { get; set; }
+
+        [JsonProperty("genres_series")]
+        public ICollection<GenresSeries> GenresSeries { get; set; }
+
+        [JsonProperty("genres_books")]
+        public ICollection<GenresBook> GenresBooks { get; set; }
+
+        [JsonProperty("genres_brands")]
+        public ICollection<GenresBrand> GenresBrands { get; set; }
+
+        [JsonProperty("genres_publishing_companies")]
+        public ICollection<GenresPublishingCompany> GenresPublishingCompanies { get; set; }
+
+        [JsonProperty("genres_authors")]
+        public ICollection<GenresAuthor> GenresAuthors { get; set; }
 
         #endregion
 
