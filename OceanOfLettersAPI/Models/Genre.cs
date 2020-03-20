a7061c3faaf95304bb20888cecf211a6679ec929
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-20 14:12:23 -0300

    Adicionando model Genre

---
+++ OceanOfLettersAPI/Models/Genre.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace OceanOfLettersAPI.Models
+{
+
+    public class Genre
+    {
+
+        #region Properties
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        #endregion
+
+    }
+
+}
