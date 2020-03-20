4135562823914aca8bc1a2a5d51ef7edf14e38aa
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-20 14:43:07 -0300

    Adicionando model PublishingCompany

---
+++ OceanOfLettersAPI/Models/PublishingCompany.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace OceanOfLettersAPI.Models
+{
+
+    public class PublishingCompany
+    {
+
+        #region Properties
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("foundation")]
+        public DateTime Foundation { get; set; }
+
+        [JsonProperty("founders")]
+        public string Founders { get; set; }
+
+        [JsonProperty("headquarters")]
+        public string Headquarters { get; set; }
+
+        [JsonProperty("website")]
+        public string Website { get; set; }
+
+        #endregion
+
+    }
+
+}
