b4a2f979940026defa9e11b1cad513a895d7f827
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-21 10:24:38 -0300

    Atualizando a classe Validations

--- OceanOfLettersAPI/Validations/Validations.cs
+++ OceanOfLettersAPI/Validations/Validations.cs
@@ -5,12 +5,15 @@ using System;
 namespace OceanOfLettersAPI.Validations
 {
 
-    public static class Validations
+    public class Validations
     {
 
-        public static Response Validate(Author author)
+        public bool IsValid { get; set; }
+
+        public Response Validate(Author author)
         {
 
+            IsValid = true;
             Response response = new Response();
 
             try
@@ -20,36 +23,42 @@ namespace OceanOfLettersAPI.Validations
                 {
                     response.BadRequest = true;
                     response.Message = "Insira o nome do autor!";
+                    IsValid = false;
                 }
 
                 if (author.Nationality == null)
                 {
                     response.BadRequest = true;
                     response.Message = "Insira a nacionalidade do autor!";
+                    IsValid = false;
                 }
 
                 if (author.Birth == null)
                 {
                     response.BadRequest = true;
                     response.Message = "Insira a data de nascimento do autor!";
+                    IsValid = false;
                 }
 
                 if (author.Biography == null)
                 {
                     response.BadRequest = true;
                     response.Message = "Insira a biografia do autor!";
+                    IsValid = false;
                 }
 
                 if (author.CountryId == null)
                 {
                     response.BadRequest = true;
                     response.Message = "Insira o país de origem do autor!";
+                    IsValid = false;
                 }
 
                 if (author.ActivityPeriod == null)
                 {
                     response.BadRequest = true;
                     response.Message = "Insira o tempo de atividade do autor!";
+                    IsValid = false;
                 }
 
             }
@@ -57,6 +66,7 @@ namespace OceanOfLettersAPI.Validations
             {
                 response.Message = ex.Message;
                 response.BadRequest = true;
+                IsValid = false;
             }
 
             return response;
