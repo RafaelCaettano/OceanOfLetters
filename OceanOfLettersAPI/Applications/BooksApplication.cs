69f7b5676d99b3f761de88b0e38ac553789ea52b
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-21 17:08:46 -0300

    Adicionando BooksApplication

---
+++ OceanOfLettersAPI/Applications/BooksApplication.cs
@@ -0,0 +1,22 @@
+﻿using OceanOfLettersAPI.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanOfLettersAPI.Applications
+{
+    public class BooksApplication
+    {
+
+        private readonly OceanOfLettersContext Context;
+
+        public BooksApplication(OceanOfLettersContext context)
+        {
+            Context = context;
+        }
+
+
+
+    }
+}
