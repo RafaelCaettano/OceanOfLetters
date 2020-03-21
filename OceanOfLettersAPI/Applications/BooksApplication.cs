07d649f5c24b6ca65592676fa9c828443e8864b3
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-21 19:19:13 -0300

    Criando método Store BooksControllers

--- OceanOfLettersAPI/Applications/BooksApplication.cs
+++ OceanOfLettersAPI/Applications/BooksApplication.cs
@@ -1,4 +1,7 @@
 ﻿using OceanOfLettersAPI.Context;
+using OceanOfLettersAPI.Models;
+using OceanOfLettersAPI.Models.Relationships;
+using OceanOfLettersAPI.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +19,185 @@ namespace OceanOfLettersAPI.Applications
             Context = context;
         }
 
+        public async Task<Response> Store(Book book)
+        {
 
+            Response response = new Response
+            {
+                Book = book
+            };
+
+            try
+            {
+
+                if (!BookExists(book.Name))
+                {
+
+                    foreach (AuthorsBook authorsBook in book.AuthorsBook)
+                    {
+
+                        var wherePublishingCompaniesAuthor = Context.PublishingCompaniesAuthor.Where(x => x.PublishingCompanyId == book.PublishingCompanyId && x.AuthorId == authorsBook.AuthorId).ToList();
+                        PublishingCompaniesAuthor publishingCompaniesAuthorExist = wherePublishingCompaniesAuthor.FirstOrDefault();
+
+                        if (publishingCompaniesAuthorExist == null)
+                        {
+                            PublishingCompaniesAuthor publishingCompaniesAuthor = new PublishingCompaniesAuthor
+                            {
+                                AuthorId = authorsBook.AuthorId,
+                                PublishingCompanyId = book.PublishingCompanyId
+                            };
+
+                            Context.PublishingCompaniesAuthor.Add(publishingCompaniesAuthor);
+                        }
+
+                        if (book.BrandId != null)
+                        {
+                            var whereBrandsAuthor = Context.BrandsAuthor.Where(x => x.BrandId == book.BrandId && x.AuthorId == authorsBook.AuthorId).ToList();
+                            BrandsAuthor brandsAuthorExist = whereBrandsAuthor.FirstOrDefault();
+
+                            if (brandsAuthorExist == null)
+                            {
+                                BrandsAuthor brandsAuthor = new BrandsAuthor
+                                {
+                                    AuthorId = authorsBook.AuthorId,
+                                    BrandId = book.BrandId
+                                };
+
+                                Context.BrandsAuthor.Add(brandsAuthor);
+                            }
+
+                        }
+
+                    }
+
+                    if (book.SeriesId != null && book.BrandId != null)
+                    {
+                        var whereBrandsSeries = Context.BrandsSeries.Where(x => x.BrandId == book.BrandId && x.SeriesId == book.SeriesId).ToList();
+                        BrandsSeries brandsSeriesExist = whereBrandsSeries.FirstOrDefault();
+
+                        if (brandsSeriesExist == null)
+                        {
+                            BrandsSeries brandsSeries = new BrandsSeries
+                            {
+                                BrandId = book.BrandId,
+                                SeriesId = book.SeriesId
+                            };
+
+                            Context.BrandsSeries.Add(brandsSeries);
+                        }
+                    }
+
+                    foreach (GenresBook genresBook in book.GenresBooks)
+                    {
+
+                        foreach (AuthorsBook authorsBook in book.AuthorsBook)
+                        {
+                            var whereGenresAuthor = Context.GenresAuthor.Where(x => x.GenreId == genresBook.GenreId && x.AuthorId == authorsBook.AuthorId).ToList();
+                            GenresAuthor genresAuthorExist = whereGenresAuthor.FirstOrDefault();
+
+                            if (genresAuthorExist == null)
+                            {
+                                GenresAuthor genresAuthor = new GenresAuthor
+                                {
+                                    AuthorId = authorsBook.AuthorId,
+                                    GenreId = genresBook.GenreId
+                                };
+
+                                Context.GenresAuthor.Add(genresAuthor);
+                            }
+                        }
+
+                        var whereGenresPublishingCompanies = Context.GenresPublishingCompany.Where(x => x.GenreId == genresBook.GenreId && x.PublishingCompanyId == book.PublishingCompanyId).ToList();
+                        GenresPublishingCompany genresPublishingCompanyExist = whereGenresPublishingCompanies.FirstOrDefault();
+
+                        if (genresPublishingCompanyExist == null)
+                        {
+                            GenresPublishingCompany genresPublishingCompany = new GenresPublishingCompany
+                            {
+                                PublishingCompanyId = book.PublishingCompanyId,
+                                GenreId = genresBook.GenreId
+                            };
+
+                            Context.GenresPublishingCompany.Add(genresPublishingCompany);
+                        }
+
+                        if (book.BrandId != null)
+                        {
+                            var whereGenresBrands = Context.GenresBrand.Where(x => x.GenreId == genresBook.GenreId && x.BrandId == book.BrandId).ToList();
+                            GenresBrand genresBrandExist = whereGenresBrands.FirstOrDefault();
+
+                            if (genresBrandExist == null)
+                            {
+                                GenresBrand genresBrand = new GenresBrand
+                                {
+                                    BrandId = book.BrandId,
+                                    GenreId = genresBook.GenreId
+                                };
+
+                                Context.GenresBrand.Add(genresBrand);
+                            }
+
+                        }
+
+                        if (book.SeriesId != null)
+                        {
+                            var whereGenresSeries = Context.GenresSeries.Where(x => x.GenreId == genresBook.GenreId && x.SeriesId == book.SeriesId).ToList();
+                            GenresSeries genresSeriesExist = whereGenresSeries.FirstOrDefault();
+
+                            if (genresSeriesExist == null)
+                            {
+                                GenresSeries genresSeries = new GenresSeries
+                                {
+                                    GenreId = genresBook.GenreId,
+                                    SeriesId = book.SeriesId
+                                };
+
+                                Context.GenresSeries.Add(genresSeries);
+                            }
+                        }
+
+                    }
+
+                    var wherePublishingCompaniesSeries = Context.PublishingCompaniesSeries.Where(x => x.SeriesId == book.SeriesId && x.PublishingCompanyId == book.PublishingCompanyId).ToList();
+                    PublishingCompaniesSeries publishingCompaniesSeriesExist = wherePublishingCompaniesSeries.FirstOrDefault();
+
+                    if (publishingCompaniesSeriesExist == null)
+                    {
+                        PublishingCompaniesSeries publishingCompaniesSeries = new PublishingCompaniesSeries
+                        {
+                            SeriesId = book.SeriesId,
+                            PublishingCompanyId = book.PublishingCompanyId
+                        };
+
+                        Context.PublishingCompaniesSeries.Add(publishingCompaniesSeries);
+                    }
+
+                    Context.Add(book);
+                    await Context.SaveChangesAsync();
+
+                    response.Message = "Livro cadastrado com sucesso!";
+                }
+                else
+                {
+                    response.Message = "Livro já cadastrado";
+                    response.BadRequest = true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.BadRequest = true;
+            }
+
+            return response;
+
+        }
+
+        private bool BookExists(string name)
+        {
+            return Context.Book.Any(e => e.Name == name);
+        }
 
     }
 }
--- OceanOfLettersAPI/Controllers/BooksController.cs
+++ OceanOfLettersAPI/Controllers/BooksController.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OceanOfLettersAPI.Applications;
 using OceanOfLettersAPI.Context;
 using OceanOfLettersAPI.Models;
+using OceanOfLettersAPI.Utilities;
 
 namespace OceanOfLettersAPI.Controllers
 {
@@ -74,10 +76,92 @@ namespace OceanOfLettersAPI.Controllers
         [HttpPost]
         public async Task<ActionResult<Book>> PostBook([FromServices] OceanOfLettersContext _context, Book book)
         {
-            _context.Book.Add(book);
-            await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetBook", new { id = book.Id }, book);
+            Response response = new Response();
+            Validations.Validations validation = new Validations.Validations();
+
+            try
+            {
+
+                response = validation.Validate(book);
+
+                if (validation.IsValid)
+                {
+
+                    //foreach (AuthorsBook authorBook in book.AuthorsBook)
+                    //{
+                    //    if (!new AuthorApplication(_context).ExistAuthorById(authorBook.AuthorId))
+                    //    {
+                    //        repost.Message = "Autor não cadastrado!";
+                    //        return BadRequest(repost);
+                    //    }
+                    //}
+
+                    //if (!new PublishingCompanyApplication(_context).ExistPublishingCompany(book.PublishingCompanyId))
+                    //{
+                    //    repost.Message = "Editora não cadastrada!";
+                    //    return BadRequest(repost);
+                    //}
+
+                    //if (book.SeriesId != null)
+                    //{
+                    //    if (!new SeriesApplication(_context).ExistSeries(book.SeriesId))
+                    //    {
+                    //        repost.Message = "Série não cadastrada!";
+                    //        return BadRequest(repost);
+                    //    }
+                    //}
+
+                    //if (book.BrandId != null)
+                    //{
+                    //    if (!new BrandApplication(_context).ExistBrand(book.BrandId))
+                    //    {
+                    //        repost.Message = "Marca de editora não cadastrada!";
+                    //        return BadRequest(repost);
+                    //    }
+                    //}
+
+                    //if (!new CountryApplication(_context).ExistCountry(book.CountryId))
+                    //{
+                    //    repost.Message = "País não cadastrado!";
+                    //    return BadRequest(repost);
+                    //}
+
+                    //if (new LanguageApplication(_context).ExistLanguage(book.LanguageId) == false)
+                    //{
+                    //    repost.Message = "Idioma não cadastrado!";
+                    //    return BadRequest(repost);
+                    //}
+
+                    //foreach (GenresBook genresBook in book.GenresBooks)
+                    //{
+                    //    if (!new GenreApplication(_context).ExistGenre(genresBook.GenreId))
+                    //    {
+                    //        repost.Message = "Gênero não cadastrado!";
+                    //        return BadRequest(repost);
+                    //    }
+                    //}
+
+                    response = await new BooksApplication(_context).Store(book);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.BadRequest = true;
+            }
+
+            if (response.BadRequest)
+                return BadRequest(response);
+            else
+                return CreatedAtAction("GetBook", new { id = book.Id }, response);
+
+            //_context.Book.Add(book);
+            //await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetBook", new { id = book.Id }, book);
         }
 
         // DELETE: api/Books/5
