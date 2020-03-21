efaeb7a8df244021c3aa320b9c65d020aa4d4db1
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-21 09:57:34 -0300

    Adicionando AuthorsAplication

---
+++ OceanOfLettersAPI/Applications/AuthorsApplication.cs
@@ -0,0 +1,22 @@
+﻿using OceanOfLettersAPI.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanOfLettersAPI.Applications
+{
+
+    public class AuthorsApplication
+    {
+
+        private readonly OceanOfLettersContext Context;
+
+        public AuthorsApplication(OceanOfLettersContext context)
+        {
+            Context = context;
+        }
+
+    }
+
+}
