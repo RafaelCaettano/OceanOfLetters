5b9e2c06c3a249e01baf36a6afddaa3e0e9f84e5
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-22 16:45:28 -0300

    Adicionando Serialize Methods a model Genre

--- OceanOfLettersAPI/Models/Genre.cs
+++ OceanOfLettersAPI/Models/Genre.cs
@@ -77,6 +77,21 @@ namespace OceanOfLettersAPI.Models
 
         #endregion
 
+        #region Serialize Methods
+
+        public bool ShouldSerializeBooks() { return Books.Count > 0; }
+        public bool ShouldSerializeAuthors() { return Authors.Count > 0; }
+        public bool ShouldSerializePublishingCompanies() { return PublishingCompanies.Count > 0; }
+        public bool ShouldSerializeBrands() { return Brands.Count > 0; }
+        public bool ShouldSerializeSeries() { return Series.Count > 0; }
+        public bool ShouldSerializeGenresBooks() { return false; }
+        public bool ShouldSerializeGenresAuthors() { return false; }
+        public bool ShouldSerializeGenresPublishingCompanies() { return false; }
+        public bool ShouldSerializeGenresBrands() { return false; }
+        public bool ShouldSerializeGenresSeries() { return false; }
+
+        #endregion
+
     }
 
 }
