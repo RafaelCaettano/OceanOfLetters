25f7c07003f5af91204ed58a6842d0b47a6622f4
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-22 19:11:25 -0300

    Adicionando PublishingCompaniesApplications

---
+++ OceanOfLettersAPI/Applications/PublishingCompaniesApplication.cs
@@ -0,0 +1,26 @@
+﻿using OceanOfLettersAPI.Context;
+using OceanOfLettersAPI.Models;
+using OceanOfLettersAPI.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanOfLettersAPI.Applications
+{
+
+    public class PublishingCompaniesApplication
+    {
+
+        private readonly OceanOfLettersContext Context;
+
+        public PublishingCompaniesApplication(OceanOfLettersContext context)
+        {
+            Context = context;
+        }
+
+        
+
+    }
+
+}
