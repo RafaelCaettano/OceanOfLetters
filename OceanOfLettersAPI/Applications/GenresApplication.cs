ead7d34a3845bc0c296a80f77e9223ad69f51a3f
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-22 16:30:48 -0300

    Adicionando GenresApplication

---
+++ OceanOfLettersAPI/Applications/GenresApplication.cs
@@ -0,0 +1,24 @@
+﻿using OceanOfLettersAPI.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanOfLettersAPI.Applications
+{
+
+    public class GenresApplication
+    {
+
+        private readonly OceanOfLettersContext Context;
+
+        public GenresApplication(OceanOfLettersContext context)
+        {
+            Context = context;
+        }
+
+
+
+    }
+
+}
