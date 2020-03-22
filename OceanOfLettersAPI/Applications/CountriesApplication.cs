15fcf5a8dc8c1759eba40eb107a607f25b909376
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-22 13:21:57 -0300

    Adicionando CountriesApplication

---
+++ OceanOfLettersAPI/Applications/CountriesApplication.cs
@@ -0,0 +1,22 @@
+﻿using OceanOfLettersAPI.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanOfLettersAPI.Applications
+{
+
+    public class CountriesApplication
+    {
+
+        private readonly OceanOfLettersContext Context;
+
+        public CountriesApplication(OceanOfLettersContext context)
+        {
+            Context = context;
+        }
+
+    }
+
+}
