e994c825ae562a7dd5592d558cfa47455b1892b5
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-22 12:54:56 -0300

    Adicionando BrandsApplication

---
+++ OceanOfLettersAPI/Applications/BrandsApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanOfLettersAPI.Applications
+{
+
+    public class BrandsApplication
+    {
+
+        private readonly OceanOfLettersContext Context;
+
+        public AuthorsApplication(OceanOfLettersContext context)
+        {
+            Context = context;
+        }
+
+    }
+
+}
