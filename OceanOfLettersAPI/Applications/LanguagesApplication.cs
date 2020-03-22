4d46e9bb9306149e6d075bad24c6a91506921e4a
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-22 17:09:14 -0300

    Adicionando LanguagesApplication

---
+++ OceanOfLettersAPI/Applications/LanguagesApplication.cs
@@ -0,0 +1,22 @@
+﻿using OceanOfLettersAPI.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanOfLettersAPI.Applications
+{
+
+    public class LanguagesApplication
+    {
+
+        private readonly OceanOfLettersContext Context;
+
+        public LanguagesApplication(OceanOfLettersContext context)
+        {
+            Context = context;
+        }
+
+    }
+
+}
