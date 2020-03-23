2c54904175df6c0715f7fd953d6b091b930f8a86
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-22 21:26:25 -0300

    Adicionando SeriesApplication

---
+++ OceanOfLettersAPI/Applications/SeriesApplication.cs
@@ -0,0 +1,24 @@
+﻿using OceanOfLettersAPI.Context;
+using OceanOfLettersAPI.Models;
+using OceanOfLettersAPI.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanOfLettersAPI.Applications
+{
+
+    public class SeriesApplication
+    {
+
+        private readonly OceanOfLettersContext Context;
+
+        public SeriesApplication(OceanOfLettersContext context)
+        {
+            Context = context;
+        }
+
+    }
+
+}
