f94521330a12d4f411d9809112978da767b92b8d
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-24 15:12:54 -0300

    Adicionando collection Books

---
+++ OceanOfLettersAPI/Collections/Books.cs
@@ -0,0 +1,54 @@
+﻿using OceanOfLettersAPI.Models;
+using OceanOfLettersAPI.Models.Relationships;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanOfLettersAPI.Collections
+{
+
+    public class Books<T> : List<Book>
+    {
+
+        public void Incorporate(List<Book> books)
+        {
+
+            Clear();
+
+            foreach (Book book in books)
+            {
+                Add(book);
+            }
+
+        }
+
+        public void Genres()
+        {
+
+            ForEach(delegate (Book book)
+            {
+
+                foreach (GenresBook genresBook in book.GenresBooks)
+                    book.Genres.Add(genresBook.Genre);
+
+            });
+
+        }
+
+        public void Authors()
+        {
+
+            ForEach(delegate (Book book)
+            {
+
+                foreach (AuthorsBook authorsBook in book.AuthorsBooks)
+                    book.Authors.Add(authorsBook.Author);
+
+            });
+
+        }
+
+    }
+
+}
