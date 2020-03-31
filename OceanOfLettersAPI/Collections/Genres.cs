2c8616c8bc1182145474b50b84017511c195a8b5
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-31 17:51:48 -0300

    Adicionando collection Genres

---
+++ OceanOfLettersAPI/Collections/Genres.cs
@@ -0,0 +1,93 @@
+﻿using OceanOfLettersAPI.Models;
+using OceanOfLettersAPI.Models.Relationships;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanOfLettersAPI.Collections
+{
+
+    public class Genres : List<Genre>
+    {
+
+        public void Incorporate(List<Genre> genres)
+        {
+
+            Clear();
+
+            foreach (Genre genre in genres)
+            {
+                Add(genre);
+            }
+
+        }
+
+        public void Books()
+        {
+
+            ForEach(delegate (Genre genre)
+            {
+
+                foreach (GenresBook genresBook in genre.GenresBooks)
+                    genre.Books.Add(genresBook.Book);
+
+            });
+
+        }
+
+        public void Series()
+        {
+
+            ForEach(delegate (Genre genre)
+            {
+
+                foreach (GenresSeries genresSeries in genre.GenresSeries)
+                    genre.Series.Add(genresSeries.Series);
+
+            });
+
+        }
+
+        public void Brands()
+        {
+
+            ForEach(delegate (Genre genre)
+            {
+
+                foreach (GenresBrand genresBrand in genre.GenresBrands)
+                    genre.Brands.Add(genresBrand.Brand);
+
+            });
+
+        }
+
+        public void Authors()
+        {
+
+            ForEach(delegate (Genre genre)
+            {
+
+                foreach (GenresAuthor genresAuthor in genre.GenresAuthors)
+                    genre.Authors.Add(genresAuthor.Author);
+
+            });
+
+        }
+
+        public void PublishingCompanies()
+        {
+
+            ForEach(delegate (Genre genre)
+            {
+
+                foreach (GenresPublishingCompany genresPublishingCompany in genre.GenresPublishingCompanies)
+                    genre.PublishingCompanies.Add(genresPublishingCompany.PublishingCompany);
+
+            });
+
+        }
+
+    }
+
+}
