1bc2a92bcf22fc76483a7d1ab16abe48c741c7f5
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-31 18:13:01 -0300

    Adicionando collection Languages

---
+++ OceanOfLettersAPI/Collections/Languages.cs
@@ -0,0 +1,28 @@
+﻿using OceanOfLettersAPI.Models;
+using OceanOfLettersAPI.Models.Relationships;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanOfLettersAPI.Collections
+{
+
+    public class Languages<T> : List<Language>
+    {
+
+        public void Incorporate(List<Language> languages)
+        {
+
+            Clear();
+
+            foreach (Language language in languages)
+            {
+                Add(language);
+            }
+
+        }
+
+    }
+
+}
