00fe2dc899332b96864502078bf32763b964cc34
CaetanoRafael <rafac.docarmo@hotmail.com>
2020-03-31 17:36:21 -0300

    Adicionando collection Countries

---
+++ OceanOfLettersAPI/Collections/Countries.cs
@@ -0,0 +1,25 @@
+﻿using OceanOfLettersAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OceanOfLettersAPI.Collections
+{
+    public class Countries<T> : List<Country>
+    {
+
+        public void Incorporate(List<Country> countries)
+        {
+
+            Clear();
+
+            foreach (Country country in countries)
+            {
+                Add(country);
+            }
+
+        }
+
+    }
+}
